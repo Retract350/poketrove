54992e41e846e81f2453a114243bde0f46f3067a
Carter Thomas <carterthomas350@gmail.com>
2023-08-31 09:57:59 -0500

    change some colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Hello world</h1>
+  <h1>Hello world<span>!</span></h1>
 </template>
 
 <script lang="ts"></script>
+
+<style lang="scss">
+h1 {
+  color: blue;
+
+  span {
+    color: red;
+  }
+}
+</style>
