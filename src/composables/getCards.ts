31950891e11277ce603bec089f37bca1d255f537
Carter <carterthomas350@gmail.com>
2023-09-02 15:01:12 -0500

    Finish search logic, add temporary form element

--- src/composables/getCards.ts
+++ src/composables/getCards.ts
@@ -1,20 +1,34 @@
 const APIKey = "f632b8f7-2990-4593-9768-584578b57697";
 
-const getCards = async (parameter: string, name: string) => {
-  try {
-    const urlParams: string = "https://api.pokemontcg.io/v2/" + parameter;
-    const url: string = name ? urlParams + "?q=name:" + name : urlParams;
+const cardsURL: string = "https://api.pokemontcg.io/v2/cards?q=name:";
+const setURL: string = "https://api.pokemontcg.io/v2/cards?q=set.name:";
 
-    const res = await fetch(url, {
-      headers: {
-        "X-Api-Key": APIKey,
-      },
-    });
+const getCards = async (parameter: number, term: string) => {
+  let queryURL: string;
 
-    let data = await res.json();
-    console.log(data);
-  } catch (err: any) {
-    console.error(err.message);
+  if (parameter === 1) {
+    queryURL = cardsURL + term;
+  } else if (parameter === 2) {
+    queryURL = setURL + term;
+  } else {
+    queryURL = "";
+  }
+
+  if (queryURL) {
+    try {
+      const res = await fetch(queryURL, {
+        headers: {
+          "X-Api-Key": APIKey,
+        },
+      });
+      const data = await res.json();
+
+      console.log(data);
+    } catch (err: any) {
+      console.error(err);
+    }
+  } else {
+    console.error("Invalid request URL");
   }
 };
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,64 @@
 <template>
-  <button @click="getCards('cards', 'piplup')">Click me</button>
+  <form @submit.prevent="getCards(isActive, searchTerm)">
+    <div @click="toggleSearchBy('card')" :class="{ active: isActive === 1 }">
+      Cards
+    </div>
+    <div @click="toggleSearchBy('set')" :class="{ active: isActive === 2 }">
+      Set
+    </div>
+    <input type="text" v-model="searchTerm" />
+    <button>submit</button>
+  </form>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import getCards from "../composables/getCards";
+
+const searchTerm = "";
+
+let isActive = ref(1);
+
+const toggleSearchBy = (param: string) => {
+  param === "card" ? (isActive.value = 1) : (isActive.value = 2);
+};
 </script>
 
 <style lang="scss">
 body {
   height: 110vh;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 2rem;
+
+  div {
+    width: 10rem;
+    height: 1rem;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &.active {
+      background-color: pink;
+    }
+  }
+
+  label {
+    color: white;
+  }
+
+  input,
+  button {
+    width: 10rem;
+  }
 }
 </style>
