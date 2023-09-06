e80bc0ef3708ce6905a99a8ed13a7e652b500164
Carter Thomas <carterthomas350@gmail.com>
2023-09-06 13:09:56 -0500

    Add sets dropdown, update toggle dropdown logic to close when changing searchBy back to cards

--- src/components/searchCards.vue
+++ src/components/searchCards.vue
@@ -21,11 +21,11 @@
 <div class="divider"></div> -->
 
   <!-- TEST BUTTON FOR SET DROPDOWN -->
-  <button @click="toggleSetsDropdown" v-if="true">view sets</button>
+  <button @click="toggleSetsDropdown">view sets</button>
 
   <div class="search-container">
     <form @submit.prevent="submitSearch">
-      <div class="search-inputs" :class="{ corner: viewSearchByDropdown }">
+      <div class="search-inputs" :class="{ cornerLeft: viewSearchByDropdown }">
         <!-- "Search By" element, default to Cards on load -->
         <a class="search-by" @click="toggleSearchByDropdown">
           {{ activeSearchBy }}
@@ -49,24 +49,37 @@
           </svg>
         </a>
 
+        <div class="column-border"></div>
+
         <!-- Search by cards text area -->
-        <input type="text" class="search-input" placeholder="Search..." />
-      </div>
+        <input
+          type="text"
+          class="search-input"
+          placeholder="Search..."
+          :class="{ cornerRight: viewSetsDropdown }"
+          v-model="searchTerm"
+          @click="toggleSetsDropdown"
+        />
 
-      <!-- Change "Search By" dropdown -->
-      <div
-        class="search-by-dropdown"
-        v-if="viewSearchByDropdown"
-        @click="toggleSearchBy(searchByCard === true ? 'set' : 'card')"
-      >
-        <a class="search-by">{{ altSearchBy }} </a>
-      </div>
+        <!-- Add both "Search By" and "Search Sets" dropdowns -->
+        <!-- Change "Search By" dropdown -->
+        <div
+          class="search-by-dropdown"
+          :class="{ hidden: !viewSearchByDropdown }"
+          @click="toggleSearchBy(searchByCard === true ? 'set' : 'card')"
+        >
+          <a class="search-by">{{ altSearchBy }} </a>
+        </div>
+        <div class="column-border"></div>
 
-      <!-- Searchable Sets dropdown -->
-      <div v-if="viewSetsDropdown">
-        <ul>
-          <li v-for="set in setArray" style="color: white">{{ set.name }}</li>
-        </ul>
+        <!-- Searchable Sets dropdown -->
+        <div v-if="viewSetsDropdown" class="sets-dropdown">
+          <ul>
+            <li v-for="set in setArray" :key="set.id">
+              {{ set.name }} ({{ set.releaseDate.substring(0, 4) }})
+            </li>
+          </ul>
+        </div>
       </div>
     </form>
   </div>
@@ -89,7 +102,7 @@ const searchByCard = ref(true);
 
 const viewSearchByDropdown = ref(false);
 
-const searchTerm = "";
+const searchTerm = ref("");
 
 const activeSearchBy = ref("Cards");
 
@@ -105,6 +118,9 @@ function toggleSearchBy(param: string): void {
     activeSearchBy.value = "Cards";
     altSearchBy.value = "Sets";
 
+    // Close sets dropdown if it is open
+    viewSetsDropdown.value = false;
+
     console.log("searching by " + activeSearchBy.value);
   } else {
     searchByCard.value = false;
@@ -118,13 +134,17 @@ function toggleSearchBy(param: string): void {
 }
 
 function submitSearch() {
-  console.log("searching");
+  console.log(searchTerm.value);
 }
 
 const viewSetsDropdown = ref(false);
 
 function toggleSetsDropdown(): void {
-  viewSetsDropdown.value = !viewSetsDropdown.value;
+  if (searchByCard.value === false) {
+    viewSetsDropdown.value = !viewSetsDropdown.value;
+  } else {
+    return;
+  }
 }
 
 onBeforeMount(getSetsOnLoad);
@@ -135,32 +155,33 @@ onBeforeMount(getSetsOnLoad);
 @import "../assets/variables.scss";
 
 .search-container {
-  width: 30rem;
+  width: 33rem;
   margin: 2rem auto;
   display: flex;
   flex-direction: column;
 }
 
 .search-inputs {
-  display: flex;
+  display: grid;
+  grid-template-columns: 20% 0.125rem 80%;
+  width: 100%;
   height: 2rem;
   background-color: $shade-white;
-  padding: 0 1rem;
+  // padding: 0 1rem;
   border-radius: $radius-medium;
 
-  &.corner {
+  &.cornerLeft {
     border-bottom-left-radius: 0;
   }
 
   .search-by {
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    // justify-content: space-between;
     height: 100%;
-    width: 20%;
-    border-right: 2px solid $bg-primary;
+    // border-right: 2px solid $bg-primary;
     font-size: $font-small;
-    padding-right: 0.75rem;
+    padding-left: 1rem;
 
     svg {
       padding-left: 0.25rem;
@@ -169,12 +190,16 @@ onBeforeMount(getSetsOnLoad);
 
   .search-input {
     height: 2rem;
-    width: 65%;
     background-color: inherit;
     border: none;
     font-size: $font-primary;
     font-family: "Martian Mono";
     padding-left: 1rem;
+    border-radius: $radius-medium;
+
+    &.cornerRight {
+      border-bottom-right-radius: 0;
+    }
 
     &:focus-visible {
       outline: none;
@@ -183,19 +208,53 @@ onBeforeMount(getSetsOnLoad);
 }
 
 .search-by-dropdown {
-  width: 22%;
+  // width: 22%;
   height: 2rem;
   background-color: $shade-white;
-  display: flex;
-  align-items: center;
-  padding: 1rem;
+  // display: flex;
+  // align-items: center;
+  // padding: 1rem;
   font-size: $font-small;
   border-radius: 0 0 $radius-medium $radius-medium;
-  border-right: 2px solid $bg-primary;
+  // border-right: 2px solid $bg-primary;
   border-top: 1px solid $shade-grey;
 }
 
 .flip {
   transform: rotateX(180deg);
+}
+
+.column-border {
+  background-color: $bg-primary;
+}
+
+.hidden {
+  visibility: hidden;
+}
+
+.sets-dropdown {
+  background-color: $shade-white;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  height: 13rem;
+  border-bottom-left-radius: $radius-medium;
+  border-bottom-right-radius: $radius-medium;
+  border-top: 1px solid $shade-grey;
+
+  ul {
+    // padding: 0.25rem 0 0.25rem 0.5rem;
+  }
+
+  li {
+    list-style: none;
+    min-height: 2rem;
+    padding: 0.5rem 0 0.5rem 0.5rem;
+    border-bottom: 1px solid $shade-grey;
+
+    &:hover {
+      background-color: $shade-light-grey;
+      cursor: pointer;
+    }
+  }
 }
 </style>
